0624b9ec11d912b26818ab5d6709fe81ca044917
BHGKing <josecantu111@gmail.com>
2023-02-26 11:38:29 -0600

    Initial Commit

--- src/week3/Finra.java
+++ src/week3/Finra.java
@@ -2,8 +2,8 @@ package week3;
 
 public class Finra {
     public static void main(String[] args) {
-
-        int num = 15;
+/*
+        int num = 30;
         boolean by3 = num % 3 == 0;
         boolean by5 = num % 5 == 0;
 
@@ -26,6 +26,49 @@ public class Finra {
         }
 
         System.out.println( by3&&by5 ?"FINRA" : by3 ?"FIN" : by5 ?"RA" : num);
+*/
+/*
+        int number = 30;
+        String result = "";
+
+        if (number %3 == 0 && number %5 == 0 || number %3 == 0 || number %5 == 0) {
+
+            if (number %3 == 0 && number %5 == 0) {
+                result = "FINRA";
+
+            } else if (number % 3 == 0 ) {
+                result = "FIN";
+
+            } else if (number % 5 == 0) {
+                result = "RA";
+
+            }
+                System.out.println(result);
+        }else {
+            System.out.println(number);
+        }
+ */
+/*
+        int number = 3;
+        String result = "";
+
+        if (number % 3 == 0 && number % 5 == 0) {
+
+            if (number % 3 == 0 && number % 5 == 0) {
+                result = "FINRA";
+            } else if (number % 3 == 0) {
+                result = "FIN";
+
+            } else if (number % 5 == 0) {
+                result = "RA";
+            }
+            System.out.println(result);
+        } else {
+            result = "Not either FIN or RA";
+        }
+ */
+
+
 
     }
 }
